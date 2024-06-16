6ebfcc254301c5d7324da9c065477d72163ae951
Cristian Prieto <me@cprieto.com>
2024-06-16 14:20:01 +0200

    Initial version going through week 4

---
+++ trees/bst.hpp
@@ -0,0 +1,58 @@
+#pragma once
+#include <functional>
+
+template<typename T>
+class bstree {
+    struct node {
+        T value;
+        node* parent = nullptr;
+        node* left = nullptr;
+        node* right = nullptr;
+
+        explicit node(T value): value(value) {}
+
+        void in_order(std::function<void(T)> action) {
+            if (left != nullptr) {
+                left->in_order(action);
+            }
+            action(value);
+            if (right != nullptr) {
+                right->in_order(action);
+            }
+        }
+    };
+
+    node* root;
+
+    node* insert_node(node* current, T value) {
+        if (current == nullptr) {
+            current = new node(value);
+            if (root == nullptr) {
+                root = current;
+            }
+        } else if (value < current->value) {
+            auto left_child = insert_node(current->left, value);
+            current->left = left_child;
+            left_child->parent = current;
+        } else {
+            auto right_child = insert_node(current->right_child, value);
+            current->right = right_child;
+            right_child->parent = current;
+        }
+
+        return current;
+    }
+
+public:
+    bstree(): root(nullptr) {}
+
+    void insert(T value) {
+        insert_node(root, value);
+    }
+
+    void foreach_in_order(std::function<void(T)> action) {
+        if (root == nullptr) return;
+
+        root->in_order(action);
+    }
+};
