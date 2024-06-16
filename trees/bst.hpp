9c9458410308afa2b384081171e1d3396cd79daa
Cristian Prieto <me@cprieto.com>
2024-06-16 21:53:46 +0200

    Tests works and pass

--- trees/bst.hpp
+++ trees/bst.hpp
@@ -1,58 +1,63 @@
 #pragma once
 #include <functional>
 
-template<typename T>
+template <typename T>
 class bstree {
-    struct node {
-        T value;
-        node* parent = nullptr;
-        node* left = nullptr;
-        node* right = nullptr;
+  struct node {
+    T value;
+    node* parent = nullptr;
+    node* left = nullptr;
+    node* right = nullptr;
 
-        explicit node(T value): value(value) {}
+    explicit node(T value) : value(value) {}
 
-        void in_order(std::function<void(T)> action) {
-            if (left != nullptr) {
-                left->in_order(action);
-            }
-            action(value);
-            if (right != nullptr) {
-                right->in_order(action);
-            }
-        }
-    };
-
-    node* root;
-
-    node* insert_node(node* current, T value) {
-        if (current == nullptr) {
-            current = new node(value);
-            if (root == nullptr) {
-                root = current;
-            }
-        } else if (value < current->value) {
-            auto left_child = insert_node(current->left, value);
-            current->left = left_child;
-            left_child->parent = current;
-        } else {
-            auto right_child = insert_node(current->right_child, value);
-            current->right = right_child;
-            right_child->parent = current;
-        }
-
-        return current;
+    ~node() {
+      delete left;
+      delete right;
     }
 
-public:
-    bstree(): root(nullptr) {}
+    void in_order(std::function<void(T)> action) {
+      if (left != nullptr) {
+        left->in_order(action);
+      }
+      action(value);
+      if (right != nullptr) {
+        right->in_order(action);
+      }
+    }
+  };
 
-    void insert(T value) {
-        insert_node(root, value);
+  node* root;
+
+  node* insert_node(node* current, T value) {
+    if (current == nullptr) {
+      current = new node(value);
+      if (root == nullptr) {
+        root = current;
+      }
+    } else if (value < current->value) {
+      auto left_child = insert_node(current->left, value);
+      current->left = left_child;
+      left_child->parent = current;
+    } else {
+      auto right_child = insert_node(current->right, value);
+      current->right = right_child;
+      right_child->parent = current;
     }
 
-    void foreach_in_order(std::function<void(T)> action) {
-        if (root == nullptr) return;
+    return current;
+  }
 
-        root->in_order(action);
-    }
+ public:
+  bstree() : root(nullptr) {}
+  ~bstree() { delete root; }
+
+  void insert(T value) { insert_node(root, value); }
+
+  void foreach_in_order(std::function<void(T)> action) {
+    if (root == nullptr)
+      return;
+
+    root->in_order(action);
+  }
 };
--- trees/btree_tests.cpp
+++ trees/btree_tests.cpp
@@ -1,4 +1,22 @@
-#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_all.hpp>
 
 #include "bst.hpp"
 
+TEST_CASE("In-order transversal", "[btree]") {
+  bstree<int> tree;
+
+  tree.insert(5);
+  tree.insert(3);
+  tree.insert(7);
+  tree.insert(2);
+  tree.insert(4);
+  tree.insert(6);
+  tree.insert(8);
+
+  std::vector<int> result;
+  tree.foreach_in_order(
+      [&result](int elem) -> void { result.push_back(elem); });
+
+  std::vector<int> expected = {2, 3, 4, 5, 6, 7, 8};
+  REQUIRE(result == expected);
+}
